727992a7bfef44ad9272e6c4e3a63b3a7b5de95e
WizzyGeek <ojasscoding@gmail.com>
2023-12-09 13:57:43 +0530

    day9: newton's difference method or something forgot name

---
+++ day9/part1.py
@@ -0,0 +1,11 @@
+from itertools import pairwise
+
+def extrapolate(ts):
+    if len(ts) == 1:
+        return ts[0]
+    if any(ts):
+        return ts[-1] + extrapolate(list(map(lambda k: k[1] - k [0], pairwise(ts))))
+    return 0
+
+with open("day9/input.txt", "r") as fp:
+    print(sum(map(lambda k: extrapolate(list(map(int, k.split()))), fp.readlines())))
