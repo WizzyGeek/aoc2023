363a4eaeb274a8450593bf9f2bdc80b7f07daf0b
WizzyGeek <ojasscoding@gmail.com>
2023-12-23 02:45:16 +0530

    day22: cleanup

--- day22/part1.py
+++ day22/part1.py
@@ -1,8 +1,8 @@
 
-from itertools import chain, product, starmap
+from itertools import starmap
+
 import numpy as np
 from wafle import mapper, rpartial, star
-
 
 a = mapper.of(open("day22/input.txt", "r").readlines()) | rpartial(str.split, "~")
 a |= star(lambda x, y: (tuple(map(int, x.split(","))), tuple(map(lambda k: int(k) + 1, y.split(",")))))
@@ -12,20 +12,12 @@ d = mapper.of(a)
 d |= star(lambda _, t: (t[0], t[1]))
 shape = d >= max
 
-# print(d)
-
 c = mapper.of(a)
 c |= lambda x: tuple(starmap(slice, zip(*x)))
-
-
-def brick_vertical(brick: list):
-    return brick[-1].start + 1 != brick[-1].stop
 
 b = mapper.of(sorted(c, key=lambda brick: brick[-1].start))
 b = b > enumerate
 b |= star(lambda idx, x: (idx + 1, x[:-1], x[-1].stop - x[-1].start))
-
-# print(b >= list)
 
 totbricks = len(a)
 
