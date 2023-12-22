b0b9cffca7ff90635a3e06664a485b12c794c806
WizzyGeek <ojasscoding@gmail.com>
2023-12-23 02:42:19 +0530

    day22: cleanup

--- day22/part2.py
+++ day22/part2.py
@@ -1,51 +1,50 @@
 
-from collections import defaultdict, deque
+from collections import defaultdict
 from functools import cache
-from itertools import chain, product, starmap
+from itertools import starmap
+from time import time
+
 from more_itertools import consume
-import numpy as np
 from wafle import mapper, rpartial, star
+import numpy as np
 
 
-a = mapper.of(open("day22/input.txt", "r").readlines()) | rpartial(str.split, "~")
+_n = time()
+
+a = mapper.of(open("day22/input.txt", "r").readlines())
+a |= rpartial(str.split, "~")
 a |= star(lambda x, y: (tuple(map(int, x.split(","))), tuple(map(lambda k: int(k) + 1, y.split(",")))))
-a = a >= list
+a = a >= list # all the cubes
 
 d = mapper.of(a)
 d |= star(lambda _, t: (t[0], t[1]))
-shape = d >= max
-
-# print(d)
+shape = d >= max # find length of required x, y
 
 c = mapper.of(a)
-c |= lambda x: tuple(starmap(slice, zip(*x)))
-
-
-def brick_vertical(brick: list):
-    return brick[-1].start + 1 != brick[-1].stop
+c |= lambda x: tuple(starmap(slice, zip(*x))) # convert our cubes to slices numpy likes
 
 b = mapper.of(sorted(c, key=lambda brick: brick[-1].start))
 b = b > enumerate
 b |= star(lambda idx, x: (idx + 1, x[:-1], x[-1].stop - x[-1].start))
-
-# print(b >= list)
+# b is brick iterator
+# each brick has an id, the xy slice, and the height
 
 totbricks = len(a)
 
-heightmap = np.zeros(shape, dtype=np.uint32)
+heightmap = np.zeros(shape, dtype=np.uint32) # height of each xy cell
 brickmap = np.zeros(shape, dtype=np.uint16) # top brick of ever cell
 
-supports: dict[int, set[int]] = defaultdict(set) # this brick supports which bricks
-ontopof: dict[int, set[int]] = defaultdict(set) # this brick is on top of?
+supports: dict[int, set[int]] = defaultdict(set) # this brick supports which bricks (x supports?)
+ontopof: dict[int, set[int]] = defaultdict(set) # this brick is on top of? (x ontopof?)
 
 for idx, xyslice, height in b:
 
-    dz = heightmap[xyslice].max()
+    maxz = heightmap[xyslice].max()
 
     # possible landing bricks
     p = brickmap[xyslice]
-    if dz != 0:
-        supp = p[heightmap[xyslice] == dz]
+    if maxz != 0:
+        supp = p[heightmap[xyslice] == maxz]
         supp = set(supp)
         ontopof[idx].update(supp)
         # set(supp) is the set of all bricks this brick rests on
@@ -56,26 +55,27 @@ for idx, xyslice, height in b:
         # if len(set(supp)) == 1:
         #     unsafe_br.add(supp[0])
 
-    heightmap[xyslice] = dz + height
+    heightmap[xyslice] = maxz + height
     brickmap[xyslice] = idx
 
 # ontopof and supports are sufficient information to compute the "falling closure"
+
+# The implementation trap here is
 # A supports [X, Y, Z, ...]
 # X, Y support B
 # A supports B
 # Y supports C, but F supports C but A does not support F
 
-# step 1, calculate the supporting closure (who are ultimately supported)
-# step 2 shake the tree (who are only supported by this?) (easy, we have ontopof)
+# step 1, calculate the supporting closure (who are ultimately supported by this brick)
+# step 2 shake the tree (who are ONLY supported by this brick?) (easy, we have ontopof)
 
 @cache
-def supportswho(idx):
+def supportswho(idx): # now we know idx supports which all nodes as a set
     acc = {idx,}
     for i in supports[idx]:
         acc |= supportswho(i)
     return acc
 
-# now we know idx supports which all nodes as a set
 
 # falling closure
 def falling(idx):
@@ -87,12 +87,4 @@ def falling(idx):
             closure.difference_update(supportswho(i))
     return closure
 
-# @cache
-# def falling(idx):
-#     acc = {idx,}
-#     for i in supports[idx]: # for every brick this brick supports
-#         if len(ontopof[i]) == 1 and idx in ontopof[i]:
-#             acc |= falling(i)
-#     return acc
-
-print(mapper.of(range(1, totbricks + 1)) | falling | len | (lambda x: x-1) >= sum)
+print(mapper.of(range(1, totbricks + 1)) | falling | len | (lambda x: x-1) >= sum, time() - _n)
